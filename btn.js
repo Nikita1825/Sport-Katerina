3bf8a4d351b8952d1c126db4c377735557437e02
Nikita1825 <nikitapostnov1825@gmail.com>
2023-11-07 14:56:50 +0200

    Мобільна версія

---
+++ btn.js
@@ -0,0 +1,33 @@
+const loadmorebtn = document.querySelector(".btn-train");
+const list = document.querySelector(".train-video");
+const train = document.querySelector(".training");
+const link = document.querySelector(".link-nav");
+const nav = document.querySelector(".nav");
+let currentItems = 3;
+
+loadmorebtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  train.style.height = "2843px";
+  const Elements = [...document.querySelectorAll(".train-video li")];
+  for (let i = currentItems; i < currentItems + 7; i++) {
+    if (Elements[i]) {
+      Elements[i].style.display = "block";
+    }
+   
+    if (Elements.lastIndexOf) {
+      loadmorebtn.style.display = "none";
+    }
+  }
+  currentItems += 7;
+});
+link.addEventListener("click", (e) => {
+  nav.style.display =  "none"
+})
+{/* <script>
+  document.querySelector(".link-nav").addEventListener("click", function (){" "}
+  {
+    // this.classList.toggle("active");
+    (document.querySelector(".nav").style.display = "none")
+  }
+  )
+</script>; */}
